02e0a5f5e7d484746d834e6d4dcf812c0331696f
ShredderG <Shr3dder@icloud.com>
2016-11-02 00:14:00 +0700

    столкновения, взаимодействие с дверьми, стрельба

--- files/o_o_hero.cpp
+++ files/o_o_hero.cpp
@@ -3,19 +3,29 @@ GM_OBJECT_o_hero::GM_OBJECT_o_hero(float GM_x, float GM_y, float GM_z)
 	GM_count++;
 	o_hero = this;
 	priority = 1;
-	solid = false;
-	persistent = false;
+	persistent = true;
 	x = GM_x;
 	y = GM_y;
 	z = GM_z;
+
 	xDir = 0;
 	yDir = 90;
+	height = 0.6;
+	ammo = 20;
+	shootDelay = 0;
+	size = 0.25;
+	speed = 0.05;
+	key = false;
 
 	mouse.move(window.width / 2, window.height / 2);
 }
 
-GM_OBJECT_o_hero::~GM_OBJECT_o_hero()
+void GM_OBJECT_o_hero::destroy()
 {
+	if (!GM_active) return;
+	GM_count--;
+	GM_active = false;
+
 	if (o_hero == this)
 	{
 		if (GM_left)  if (GM_left->GM_id() == GM_id()) o_hero = (GM_OBJECT_o_hero*)GM_left;
@@ -24,12 +34,31 @@ GM_OBJECT_o_hero::~GM_OBJECT_o_hero()
 	}
 }
 
-void GM_OBJECT_o_hero::destroy()
+// collision with wall or door
+bool GM_OBJECT_o_hero::placeFree(float x, float y)
 {
-	if (!GM_active) return;
-	GM_count--;
-	GM_active = false;
+	with(o_wall)
+	{
+		if (x + size > o_wall->x)
+			if (y + size > o_wall->y)
+				if (x - size < o_wall->x + 1)
+					if (y - size < o_wall->y + 1)
+						return false;
+	}
+
+	with(o_door)
+	{
+		if (!o_door->opened || o_door->position != 1)
+			if (x + size > o_door->x)
+				if (y + size > o_door->y)
+					if (x - size < o_door->x + 1)
+						if (y - size < o_door->y + 1)
+							return false;
+	}
+
+	return true;
 }
+
 
 void GM_OBJECT_o_hero::GM_step()
 {
@@ -43,7 +72,7 @@ void GM_OBJECT_o_hero::GM_step()
 	if (yDir > 170) yDir = 170;
 
 	// Direction
-	float direction = NAN;
+	float direction = -1;
 	if (keyboard.check['W'])
 	{
 		if (!keyboard.check['S'])
@@ -66,10 +95,78 @@ void GM_OBJECT_o_hero::GM_step()
 	else if (keyboard.check['D']) direction = xDir - 90;
 
 	// Move
-	if (!isnan(direction))
+	if (direction != -1)
 	{
-		x += stepX(0.1, direction);
-		y += stepY(0.1, direction);
+		float
+			xPrevious = x,
+			yPrevious = y;
+
+		x += stepX(speed, direction);
+		y += stepY(speed, direction);
+
+		// Collision with wall
+		if (!placeFree(x, y))
+		{
+			if (placeFree(xPrevious, y)) x = xPrevious;
+			else if (placeFree(x, yPrevious)) y = yPrevious;
+			else {
+				x = xPrevious;
+				y = yPrevious;
+			}
+		}
+	}
+
+	// shooting
+	if (mouse.pressed[MOUSE_LEFT])
+	{
+		if (shootDelay <= 0)
+		{
+			if (ammo > 0)
+			{
+				objectCreate(
+					x + stepX(size, xDir, yDir),
+					y + stepY(size, xDir, yDir),
+					z + stepZ(size, xDir, yDir) + height - 0.1,
+					o_bullet);
+
+				o_bullet->xDir = xDir;
+				o_bullet->yDir = yDir;
+
+				shootDelay = 10;
+				ammo--;
+			}
+		}
+	}
+	if (shootDelay > 0) shootDelay--;
+
+	// opening doors
+	if (keyboard.pressed['E'])
+	{
+		with(o_door)
+		{
+			float
+				dx = o_door->x + 0.5,
+				dy = o_door->y + 0.5;
+
+			if (pointDistance(x, y, dx, dy) < 2.0)
+			{
+				if (o_door->locked)
+				{
+					if (key)
+					{
+						o_door->locked = !o_door->locked;
+						o_door->opened = !o_door->opened;
+						key = false;
+					}
+					else showMessage("Need key!"); // надо поменять
+				}
+				else
+				{
+					o_door->opened = !o_door->opened;
+				}
+				break;
+			}
+		}
 	}
 
 	// End
@@ -79,7 +176,7 @@ void GM_OBJECT_o_hero::GM_step()
 
 void GM_OBJECT_o_hero::GM_draw()
 {
-	view.set(x, y, z + 2, xDir, yDir, 0);
+	view.set(x, y, z + height, xDir, yDir, 0);
 }
 
 uint GM_OBJECT_o_hero::GM_id()
--- files/o_o_hero.h
+++ files/o_o_hero.h
@@ -1,14 +1,21 @@
 struct GM_OBJECT_o_hero : public GM_object
 {
 	float xDir, yDir;
+	float height;
+	float size;
+	int ammo;
+	int shootDelay;
+	float speed;
+	bool key;
 
 	static int GM_count;
 	GM_OBJECT_o_hero(float GM_x, float GM_y, float GM_z);
-	~GM_OBJECT_o_hero();
 	void destroy();
 	void GM_step();
 	void GM_draw();
 	uint GM_id();
+
+	bool placeFree(float x, float y);
 
 } *o_hero = (GM_OBJECT_o_hero*)GM_OBJECT_ID_o_hero;
 
