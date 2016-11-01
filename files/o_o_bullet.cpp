b1e3a94b6a3113789ca78f75ba1987b828d8b114
ShredderG <Shr3dder@icloud.com>
2016-11-02 00:13:18 +0700

    летит, отображается и уничтожается

--- files/o_o_bullet.cpp
+++ files/o_o_bullet.cpp
@@ -3,21 +3,14 @@ GM_OBJECT_o_bullet::GM_OBJECT_o_bullet(float GM_x, float GM_y, float GM_z)
 	GM_count++;
 	o_bullet = this;
 	priority = 0;
-	solid = false;
 	persistent = false;
 	x = GM_x;
 	y = GM_y;
 	z = GM_z;
-}
 
-GM_OBJECT_o_bullet::~GM_OBJECT_o_bullet()
-{
-	if (o_bullet == this)
-	{
-		if (GM_left)  if (GM_left->GM_id() == GM_id()) o_bullet = (GM_OBJECT_o_bullet*)GM_left;
-		if (GM_right) if (GM_right->GM_id() == GM_id()) o_bullet = (GM_OBJECT_o_bullet*)GM_right;
-		if (o_bullet == this) o_bullet = (GM_OBJECT_o_bullet*)GM_id();
-	}
+	xDir = yDir = 0;
+	speed = 0.1;
+	size = 0.05;
 }
 
 void GM_OBJECT_o_bullet::destroy()
@@ -26,14 +19,74 @@ void GM_OBJECT_o_bullet::destroy()
 	GM_count--;
 	GM_active = false;
 
+	if (o_bullet == this)
+	{
+		if (GM_left)  if (GM_left->GM_id() == GM_id()) o_bullet = (GM_OBJECT_o_bullet*)GM_left;
+		if (GM_right) if (GM_right->GM_id() == GM_id()) o_bullet = (GM_OBJECT_o_bullet*)GM_right;
+		if (o_bullet == this) o_bullet = (GM_OBJECT_o_bullet*)GM_id();
+	}
 }
 
 void GM_OBJECT_o_bullet::GM_step()
 {
+	x += stepX(speed, xDir, yDir);
+	y += stepY(speed, xDir, yDir);
+	z += stepZ(speed, xDir, yDir);
+
+	// collision with floor/roof
+	if (z < 0) destroy();
+	if (z > 1) destroy();
+
+	// collision with wall
+	with(o_wall)
+	{
+		if (x > o_wall->x)
+			if (y > o_wall->y)
+				if (x < o_wall->x + 1)
+					if (y < o_wall->y + 1)
+					{
+						destroy();
+						break;
+					}
+	}
+
+	// collision with door
+	with(o_door)
+	{
+		if (!o_door->opened || o_door->position != 1)
+			if (x > o_door->x)
+				if (y > o_door->y)
+					if (x < o_door->x + 1)
+						if (y < o_door->y + 1)
+						{
+							destroy();
+							break;
+						}
+	}
 }
 
 void GM_OBJECT_o_bullet::GM_draw()
 {
+	glDisable(GL_TEXTURE_2D); // можно удалить, если добавить текстуру
+
+	float
+		x1 = x - stepX(size, o_hero->xDir + 90),
+		x2 = x + stepX(size, o_hero->xDir + 90),
+		y1 = y - stepY(size, o_hero->xDir + 90),
+		y2 = y + stepY(size, o_hero->xDir + 90),
+		z1 = z - size,
+		z2 = z + size;
+
+	// текстуру надо добавить
+	glBegin(GL_QUADS);
+	glColor4f(1, 1, 1, 1);
+	glVertex3f(x1, y1, z1);
+	glVertex3f(x1, y1, z2);
+	glVertex3f(x2, y2, z2);
+	glVertex3f(x2, y2, z1);
+	glEnd();
+
+	glEnable(GL_TEXTURE_2D); // удалить
 }
 
 uint GM_OBJECT_o_bullet::GM_id()
--- files/o_o_bullet.h
+++ files/o_o_bullet.h
@@ -1,9 +1,11 @@
 struct GM_OBJECT_o_bullet : public GM_object
 {
+	float xDir, yDir;
+	float speed;
+	float size;
 
 	static int GM_count;
 	GM_OBJECT_o_bullet(float GM_x, float GM_y, float GM_z);
-	~GM_OBJECT_o_bullet();
 	void destroy();
 	void GM_step();
 	void GM_draw();
