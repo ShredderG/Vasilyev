3a5afb9f1fe540c7a907a8326a2a4859f201a6d1
ShredderG <Shr3dder@icloud.com>
2016-11-02 00:10:30 +0700

    нарисовал дверь и открытие/закрытие

--- files/o_o_door.cpp
+++ files/o_o_door.cpp
@@ -3,15 +3,23 @@ GM_OBJECT_o_door::GM_OBJECT_o_door(float GM_x, float GM_y, float GM_z)
 	GM_count++;
 	o_door = this;
 	priority = 0;
-	solid = false;
 	persistent = false;
 	x = GM_x;
 	y = GM_y;
 	z = GM_z;
+
+	texture = 0;
+	locked = false;
+	opened = false;
+	position = 0;
 }
 
-GM_OBJECT_o_door::~GM_OBJECT_o_door()
+void GM_OBJECT_o_door::destroy()
 {
+	if (!GM_active) return;
+	GM_count--;
+	GM_active = false;
+
 	if (o_door == this)
 	{
 		if (GM_left)  if (GM_left->GM_id() == GM_id()) o_door = (GM_OBJECT_o_door*)GM_left;
@@ -20,19 +28,25 @@ GM_OBJECT_o_door::~GM_OBJECT_o_door()
 	}
 }
 
-void GM_OBJECT_o_door::destroy()
-{
-	if (!GM_active) return;
-	GM_count--;
-	GM_active = false;
-}
-
 void GM_OBJECT_o_door::GM_step()
 {
+	if (opened)
+	{
+		if (position < 1) position += 0.05;
+	}
+	else
+	{
+		if (position > 0) position -= 0.05;
+	}
 }
 
 void GM_OBJECT_o_door::GM_draw()
 {
+	drawWall(x + 1, y, z + position, x, y, z + 1 + position);
+	drawWall(x + 1, y + 1, z + position, x + 1, y, z + 1 + position);
+	drawWall(x, y + 1, z + position, x + 1, y + 1, z + 1 + position);
+	drawWall(x, y, z + position, x, y + 1, z + 1 + position);
+	// надо низ двери нарисовать еще
 }
 
 uint GM_OBJECT_o_door::GM_id()
--- files/o_o_door.h
+++ files/o_o_door.h
@@ -1,9 +1,24 @@
 struct GM_OBJECT_o_door : public GM_object
 {
+	int texture;
+	bool locked;
+	bool opened;
+	float position;
+
+	void drawWall(float x1, float y1, float z1, float x2, float y2, float z2)
+	{
+		glBegin(GL_QUADS);
+
+		glTexCoord2f(t_floor.x1[texture], t_floor.y1[texture]); glVertex3f(x1, y1, z1);
+		glTexCoord2f(t_floor.x2[texture], t_floor.y1[texture]); glVertex3f(x1, y1, z2);
+		glTexCoord2f(t_floor.x2[texture], t_floor.y2[texture]); glVertex3f(x2, y2, z2);
+		glTexCoord2f(t_floor.x1[texture], t_floor.y2[texture]); glVertex3f(x2, y2, z1);
+
+		glEnd();
+	}
 
 	static int GM_count;
 	GM_OBJECT_o_door(float GM_x, float GM_y, float GM_z);
-	~GM_OBJECT_o_door();
 	void destroy();
 	void GM_step();
 	void GM_draw();
