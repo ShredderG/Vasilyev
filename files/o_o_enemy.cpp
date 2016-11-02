c567706b2cdaeb60ed1318ce2afb16df09a3025a
ShredderG <Shr3dder@icloud.com>
2016-11-02 21:19:15 +0700

    Получение урона

--- files/o_o_enemy.cpp
+++ files/o_o_enemy.cpp
@@ -7,6 +7,8 @@ GM_OBJECT_o_enemy::GM_OBJECT_o_enemy(float GM_x, float GM_y, float GM_z)
 	x = GM_x;
 	y = GM_y;
 	z = GM_z;
+
+	hp = HP_MAX;
 }
 
 void GM_OBJECT_o_enemy::destroy()
@@ -20,6 +22,44 @@ void GM_OBJECT_o_enemy::destroy()
 		if (GM_left)  if (GM_left->GM_id() == GM_id()) o_enemy = (GM_OBJECT_o_enemy*)GM_left;
 		if (GM_right) if (GM_right->GM_id() == GM_id()) o_enemy = (GM_OBJECT_o_enemy*)GM_right;
 		if (o_enemy == this) o_enemy = (GM_OBJECT_o_enemy*)GM_id();
+	}
+}
+
+// collision with wall or door
+bool GM_OBJECT_o_enemy::placeFree(float x, float y)
+{
+	with(o_wall)
+	{
+		if (x + SIZE > o_wall->x)
+			if (y + SIZE > o_wall->y)
+				if (x - SIZE < o_wall->x + 1)
+					if (y - SIZE < o_wall->y + 1)
+						return false;
+	}
+
+	with(o_door)
+	{
+		if (!o_door->opened || o_door->position != 1)
+			if (x + SIZE > o_door->x)
+				if (y + SIZE > o_door->y)
+					if (x - SIZE < o_door->x + 1)
+						if (y - SIZE < o_door->y + 1)
+							return false;
+	}
+
+	return true;
+}
+
+// taking damage
+void GM_OBJECT_o_enemy::getDamage(int damage)
+{
+	hp -= damage;
+
+	if (hp < 0)
+	{
+		hp = 0;
+		showMessage("Enemy killed!"); // надо поменять
+		destroy();
 	}
 }
 
--- files/o_o_enemy.h
+++ files/o_o_enemy.h
@@ -1,5 +1,10 @@
 struct GM_OBJECT_o_enemy : public GM_object
 {
+	int hp;
+
+	const float SIZE = 0.25;
+	const float HEIGHT = 0.6;
+	const int HP_MAX = 100;
 
 	static int GM_count;
 	GM_OBJECT_o_enemy(float GM_x, float GM_y, float GM_z);
@@ -7,6 +12,9 @@ struct GM_OBJECT_o_enemy : public GM_object
 	void GM_step();
 	void GM_draw();
 	uint GM_id();
+
+	bool placeFree(float x, float y);
+	void getDamage(int damage);
 
 } *o_enemy = (GM_OBJECT_o_enemy*)GM_OBJECT_ID_o_enemy;
 
